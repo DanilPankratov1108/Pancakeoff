452c425b455a94ca1381ce6c062bfe75ea7d8eb4
DanilPankratov1108 <90336955+DanilPankratov1108@users.noreply.github.com>
2022-05-29 11:57:06 +0300

    Update raspred.py

--- spring/raspred.py
+++ spring/raspred.py
@@ -1,14 +1,12 @@
 
 import pandas
-import seaborn as sns
-import numpy as np
 import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
 
-df1 = pandas.read_excel("Температура.xlsx")
-s = pandas.Series.dropna(df1["T"])
+d1 = pandas.read_excel("Температура.xlsx")
+s = pandas.Series.dropna(d1["T"])
 print(s)
-sns.histplot(s)
-print(stats.kstest(s, 'norm',))
+
+print(stats.kstest(list(s), 'norm',(s.mean(), s.std())))
 plt.show()
