9cfc93ddd1567010e2d0625876da049fe4f50e5b
DanilPankratov1108 <90336955+DanilPankratov1108@users.noreply.github.com>
2022-05-31 21:48:10 +0300

    Update raspred.py

--- spring/raspred.py
+++ spring/raspred.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
 
-d1 = pandas.read_excel("Температура.xlsx")
+d1 = pandas.read_excel("temperature.xlsx")
 s = pandas.Series.dropna(d1["T"])
 print(s)
 print(stats.kstest(list(s), 'norm',(s.mean(), s.std())))
